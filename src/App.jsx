724b2d2ab70818078f7bcbea8afa791bb82ffb72
Lucky Patil <luckypatil577R@gmail.com>
2026-02-19 12:12:06 +0530

    Implement AnimatedCounter components, and initialize application structure.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Projects from "./pages/Projects";
+import AnimatedCounter from "./components/counter/AnimaredCounter";
 
 function App() {
   const [introFinished, setIntroFinished] = useState(false);
@@ -10,6 +11,7 @@ function App() {
     <div className="min-h-screen bg-black text-white selection:bg-cyan-500 selection:text-cyan-900">
       <Navbar show={introFinished} />
       <Hero onComplete={() => setIntroFinished(true)} />
+        <AnimatedCounter />
       <Projects />
     </div>
   );
