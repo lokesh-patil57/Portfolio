1734a34073f1a711842849e5cd3f19913158b1d2
lokesh-patil57 <luckypatil577r@gmail.com>
2026-02-26 23:48:29 +0530

    new index file for constant

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -1,19 +1,278 @@
-export const counterItems = [
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Socials",
+    link: "#socials",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
   { value: 2, suffix: "+", label: "Years of Experience" },
   { value: 3, suffix: "+", label: "Hackathon Count" },
   { value: 10, suffix: "+", label: "Completed Projects" },
   { value: 95, suffix: "%", label: "Project Completion Rate" },
 ];
-export const logoIconsList = [
-  { imgPath: "/src/assets/logo1.png" },
-  { imgPath: "/src/assets/logo2.png" },
-  { imgPath: "/src/assets/logo3.png" },
-  { imgPath: "/src/assets/logo4.png" },
-  { imgPath: "/src/assets/logo5.png" },
-  { imgPath: "/src/assets/logo6.png" },
-  { imgPath: "/src/assets/logo7.png" },
-  { imgPath: "/src/assets/logo8.png" },
-  { imgPath: "/src/assets/logo9.png" },
-  { imgPath: "/src/assets/logo10.png" },
-  { imgPath: "/src/assets/logo11.png" },
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
 ];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality Focus",
+    desc: "Delivering high-quality results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Completion",
+    desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React.js Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  },
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Python Developer",
+    modelPath: "/models/python-transformed.glb",
+    scale: 0.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Backend Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
+const expCards = [
+  {
+    review: "Lokesh started working on frontend coding like HTML , CSS and JavaScript for significantly improving my frontend performance. My work has been invaluable in delivering faster experiences.",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    title: "Frontend Developer",
+    date: "January 2024 - Present",
+    responsibilities: [
+      "Developed and maintained user-facing features for the clone website like Netfilx AirBnb.",
+      "Collaborated closely with UI/UX designers students to ensure seamless user experiences.",
+      "Optimized web applications for maximum speed and scalability.",
+    ],
+  },
+  {
+    review: "Started learning Python language for learning coding in python and to get understanding about Artificial Intelligence , Generative AI .",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/logo2.png",
+    title: "Learning Python Language",
+    date: "December 2024 - February 2025",
+    responsibilities: [
+      "Understanding about core concepts , Work with libraries & frameworks",
+      "Commit to regular practice and continuous learning , Understand that debugging and problem-solving take time",
+      "Tryed to build real projects , like in CyberAware project Implemented a Python Chatbot",
+    ],
+  },
+  {
+    review: "Lokesh started Learning about the concept of Backend in MERN Stack , learning about the concept of Databases , Freamework , Runtime environment and concept about React languages.",
+    imgPath: "/images/exp3.png",
+    logoPath: "/images/logo3.png",
+    title: "MERN Stack Developer",
+    date: "March 2025 - July 2025",
+    responsibilities: [
+      "Built cross-platform Web application using React , TailwindCSS integrating with backend services.",
+      "Improved app performance and user experience through code optimization and testing.",
+      "Coordinated with the product team to implement features based on feedback.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const socials = [
+  {
+    name: "Solution Challenge 2025",
+    mentions: "@GDGCsolutionchallenge2025",
+    review:
+      "💻Worked on Problem statement : GenAI-Powered Financial Assistant for Better Investing Decisions .We are building a GenAI based solution to allow people to have a conversation about their financial needs and be better informed while making a decision. Where we built web application on hashtag #MERNstack Project Overview",
+    imgPath: "/images/client1.png",
+  },
+  // {
+  //   name: "SIH 2024",
+  //   mentions: "@wadewarren",
+  //   review:
+  //     "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+  //   imgPath: "/images/client3.png",
+  // },
+  {
+    name: "SIH 2024 Finalist",
+    mentions: "@smartindiahackathon2024",
+    review:
+      "A comprehensive AYUSH Startup Registration Portal to 	streamline  the  registration  process  for  startups  in  the  AYUSH  sector,  enhancing 	efficiency, transparency, and accessibility. we created a AYUSH web Portal with MERN stack , I handle the debugging , middleware and Error handling in our website . Designed the landing page UI design.",
+    imgPath: "/images/client2.png",
+  },
+  // {
+  //   name: "Marvin McKinney",
+  //   mentions: "@marvinmckinney",
+  //   review:
+  //     "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+  //   imgPath: "/images/client5.png",
+  // },
+  {
+    name: "Road Safety Hackathon 2025",
+    mentions: "@roadsafetyhackathon2025",
+    review:
+      "💻Worked on Problem statement : Strategy for Enhancing Foot Over Bridge (FOB) Utilization.💡Where Our Objective was creating strategic solutions that aim to improve the overall utilization of Foot Over Bridges (FOBs) by addressing the behavioral, structural, and situational challenges faced by users.",
+    imgPath: "/images/client4.png",
+  },
+  // {
+  //   name: "Albert Flores",
+  //   mentions: "@albertflores",
+  //   review:
+  //     "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+  //   imgPath: "/images/client6.png",
+  // },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    url : "https://www.instagram.com/lucky_patil.57",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "fb",
+    url : "https://www.facebook.com/lokeshjagdish.patil",
+    imgPath: "/images/fb.png",
+  },
+  {
+    name: "x",
+    url : "https://x.com/Luckypatil_57",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    url : "https://www.linkedin.com/in/lokesh-patil-91a64a320",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  socials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
