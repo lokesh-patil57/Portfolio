e08de58537ac8eb9c79bea4ed3149d52ba99cc95
Lucky Patil <luckypatil577R@gmail.com>
2026-02-28 08:50:53 +0530

    contact page , its experience in 3d and computer file

---
+++ src/components/Models/Contact/Computer.jsx
@@ -0,0 +1,32 @@
+import { useGLTF } from "@react-three/drei";
+
+export function Computer(props) {
+  const { nodes, materials } = useGLTF(
+    "/models/computer-optimized-transformed.glb"
+  );
+
+  return (
+    <group {...props} dispose={null}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <group position={[-4.005, 67.549, 58.539]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube000_ComputerDesk_0001_1.geometry}
+          material={materials["ComputerDesk.001"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Cube000_ComputerDesk_0001_2.geometry}
+          material={materials["FloppyDisk.001"]}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/models/computer-optimized-transformed.glb");
+
+export default Computer;
